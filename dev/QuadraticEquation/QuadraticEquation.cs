5aaa8a4638bbefe5708088d49172d66636c0198b
Kate <32255660+YanchukK@users.noreply.github.com>
2019-05-02 18:18:14 +0300

    Update QuadraticEquation.cs

--- dev/QuadraticEquation/QuadraticEquation.cs
+++ dev/QuadraticEquation/QuadraticEquation.cs
@@ -1,49 +1,49 @@
 class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string A, B, C;
+        double a, b, c, x, D;
+        bool result;
+
+        do
         {
-            string A, B, C;
-            double a, b, c, x, D;
-            bool result;
+            Console.WriteLine("Input a");
+            A = Console.ReadLine();
+            result = Double.TryParse(A, out a);
+        }
+        while (!result);
 
-            do
-            {
-                Console.WriteLine("Input a");
-                A = Console.ReadLine();
-                result = Double.TryParse(A, out a);
-            }
-            while (!result);
+        do
+        {
+            Console.WriteLine("Input b");
+            B = Console.ReadLine();
+            result = Double.TryParse(B, out b);
+        }
+        while (!result);
 
-            do
-            {
-                Console.WriteLine("Input b");
-                B = Console.ReadLine();
-                result = Double.TryParse(B, out b);
-            }
-            while (!result);
+        do
+        {
+            Console.WriteLine("Input c");
+            C = Console.ReadLine();
+            result = Double.TryParse(C, out c);
+        }
+        while (!result);
 
-            do
-            {
-                Console.WriteLine("Input c");
-                C = Console.ReadLine();
-                result = Double.TryParse(C, out c);
-            }
-            while (!result);
-
-            D = b * b - 4 * a * c;
+        D = b * b - 4 * a * c;
             
-            if (D < 0)
-            {
-                Console.WriteLine(" There is no solution");
-            }
-             else if(D == 0)
-            {
-                Console.WriteLine("x = "+ (-b) /( 2*a));
-            }
-            else
-            {
-                Console.WriteLine("x1 = "+ ((-b) + Math.Sqrt(D))/(2*a));
-                Console.WriteLine("x2 = " + ((-b) - Math.Sqrt(D)) / (2 * a));
-            }
+        if (D < 0)
+        {
+            Console.WriteLine(" There is no solution");
+        }
+        else if(D == 0)
+        {
+            Console.WriteLine("x = "+ (-b) /( 2*a));
+        }
+        else
+        {
+            Console.WriteLine("x1 = "+ ((-b) + Math.Sqrt(D))/(2*a));
+            Console.WriteLine("x2 = " + ((-b) - Math.Sqrt(D)) / (2 * a));
         }
     }
+}
