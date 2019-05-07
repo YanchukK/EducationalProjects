4df015f7b664941c486601d0c9d5e7bc3b2926bc
Kate <32255660+YanchukK@users.noreply.github.com>
2019-05-07 15:25:33 +0300

    Create task19.cs

---
+++ dev/OnlyMath/task19.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Millionaire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input size of array: ");
+            string[] array = new string[int.Parse(Console.ReadLine())];
+
+            Console.WriteLine("Fill the array");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Console.ReadLine();
+            }
+
+            Console.Write("Input the word: ");
+            string word = Console.ReadLine();
+
+            int counter = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    if (array[i][j] == word[counter])
+                    {
+                        counter++;
+                    }
+                    else
+                    {
+                        counter = 0;
+                    }
+
+                    if (counter == word.Length)
+                    {
+                        Console.WriteLine(j - (word.Length - 1));
+                        Console.WriteLine(array[i]);
+                    }
+                }
+            }
+        }
+    }
+}
