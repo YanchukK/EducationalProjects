fe2a69c2e35c0525ee89161efcf025af77352aef
Kate <32255660+YanchukK@users.noreply.github.com>
2019-05-09 22:28:50 +0300

    Rename task24.cs to task34.cs

---
+++ dev/OnlyMath/task34.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Millionaire
+{
+    class Program
+    {
+        enum Suit
+        {
+            Diamonds, // Бубны
+            Hearts, //  Червы/черви
+            Spades, //  Пики
+            Clubs //  Трефы
+        }
+
+        enum Value
+        {
+            Six = 6,
+            Seven,
+            Eight,
+            Nine,
+            Ten,
+            Jack = 2,
+            Lady,
+            King,
+            Ace = 11 
+        }
+
+        struct Card
+        {
+            public Suit Suit;
+            public Value Value; // ценность карты
+            //public string Name; // имя 6, 7, 8, 9, 10, валет - 2, дама - 3, король - 4, туз - 11
+        }
+
+        static void Main(string[] args)
+        {
+            // 34. Перемешать колоду карт
+
+            Card[] cards = new Card[36];
+
+            int s = 0;
+
+            for (int i = 0; i < 36;)
+            {
+                for (int j = 11; j < (2 + 10); j++) // j = 11, потому что первый в колоде тух
+                {
+                    cards[i] = new Card { Suit = (Suit)s, Value = (Value)j };
+                    if (j == 11) // потом идут 6, 7, 8, 9, 10
+                    {
+                        j = 5;
+                    }
+                    if (j == 10) // потом валет, дама, король
+                    {
+                        j = 1;
+                    }
+                    if (j == 4)
+                    {
+                        j = 2 + 10;
+                    }
+                    i++;
+                }
+                s++;
+            }
+
+            Card[] mixedcards = new Card[36];
+            Random random = new Random(); 
+            int[] randmas = new int[36]; // массив для проверки, существует
+                                         // ли уже такое сгенерированное число
+            int v = 0;
+
+            for (int i = 0; i < randmas.Length; i++)
+            {
+                if (IsEquals(v = random.Next(0, 36), 0))
+                {
+                    mixedcards[i] = cards[v];
+                    randmas[i] = v;
+                }
+                else if (!IsContains(randmas, v = random.Next(0, 36)))
+                {
+                    mixedcards[i] = cards[v];
+                    randmas[i] = v;
+                }
+                else
+                {
+                    i--;
+                }
+            }
+
+            bool IsEquals(int v1, int v2)
+            {
+                if(v1 == v2)
+                {
+                    return true;
+                }
+                return false;
+            }
+
+            bool IsContains(int[] array, int value)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] == value)
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        }
+    }
+}
