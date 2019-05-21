54873f538e29fd780a607d60f392327b327cb481
Kate <32255660+YanchukK@users.noreply.github.com>
2019-05-21 18:18:26 +0300

    Create TicTacToe.cs

---
+++ dev/Graphics/TicTacToe.cs
@@ -0,0 +1,120 @@
+using NConsoleGraphics;
+using System;
+using System.Threading;
+using System.Drawing;
+
+namespace Painting
+{
+    class Point
+    {
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        
+        public int X { get; }
+        public int Y { get; }
+    }
+
+    class Line
+    {
+        private readonly Point start, end;
+        private readonly uint color;
+        private readonly float thickness;
+
+        public Line(Point start, Point end, uint color, float thickness = 7)
+        {
+            this.start = start;
+            this.end = end;
+            this.color = color;
+            this.thickness = thickness;
+        }
+
+        public void Render(ConsoleGraphics graphics)
+        {
+            graphics.DrawLine(color, start.X, start.Y, end.X, end.Y, thickness);
+        }
+    }
+
+
+    class Cross
+    {
+        private readonly Line[] lines;
+
+        public Cross()
+        {
+            lines = new Line[2]
+            {
+                new Line(new Point(0, 0), new Point(100, 100), 0xFF00FF00),
+                new Line(new Point(0, 100), new Point(100, 0), 0xFF00FF00)
+            };
+        }
+
+        public void Render(ConsoleGraphics graphics)
+        {
+            for (int i = 0; i < lines.Length; i++)
+            {
+                lines[i].Render(graphics);
+            }
+        }
+    }
+
+    class Field
+    {
+        private readonly Line[] lines;
+
+        public Field()
+        {
+            lines = new Line[4]
+            {
+                new Line(new Point(100, 0), new Point(100, 300), 0xFF00FF00),
+                new Line(new Point(200, 0), new Point(200, 300), 0xFF00FF00),
+                new Line(new Point(0, 100), new Point(300, 100), 0xFF00FF00),
+                new Line(new Point(0, 200), new Point(300, 200), 0xFF00FF00)
+            };
+        }
+
+        public void Render(ConsoleGraphics graphics)
+        {
+            for (int i = 0; i < lines.Length; i++)
+            {
+                lines[i].Render(graphics);
+            }
+        }
+
+        public void Update(Cross cross)
+        {
+
+        }
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            ConsoleGraphics graphics = new ConsoleGraphics();
+
+            Field field = new Field();
+            Cross cross = new Cross();
+
+            while (true)
+            {
+                graphics.FillRectangle(0xFFFFFFFF, 0, 0, graphics.ClientWidth, graphics.ClientHeight);
+                graphics.DrawString("0", "Arial", 0xFF00FFFF, 200, 500, 50);
+                field.Render(graphics);
+                if (Input.IsMouseLeftButtonDown)
+                {
+                    cross.Render(graphics);
+                }
+
+                graphics.FlipPages();
+                //Thread.Sleep(50);
+            }
+        }
+    }
+}
