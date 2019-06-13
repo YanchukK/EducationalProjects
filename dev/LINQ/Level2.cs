18401cf86e2f1b9c572e2193f5b6e14fc8dc3313
Kate <32255660+YanchukK@users.noreply.github.com>
2019-06-13 18:41:55 +0300

    Update Level2.cs

--- dev/LINQ/Level2.cs
+++ dev/LINQ/Level2.cs
@@ -62,9 +62,9 @@ namespace Painting
             //    Console.WriteLine(item);
             //}
 
-
             // 2. Output all actors names
-            //Console.WriteLine(string.Join(Environment.NewLine, data.OfType<Film>().SelectMany(f => f.Actors).Select(k => k.Name)));
+            //Console.WriteLine(string.Join(Environment.NewLine,
+            //    data.OfType<Film>().SelectMany(f => f.Actors).Select(k => k.Name).Distinct()));
 
             // 3. Output number of actors born in august
             //Console.WriteLine(data.OfType<Film>().SelectMany(f => f.Actors).
@@ -77,11 +77,50 @@ namespace Painting
             // 5. Output number of books per authors
             //Console.WriteLine(string.Join(Environment.NewLine,
             //    data.OfType<Book>().GroupBy(f => f.Author).Select(f => f.Key + ": " + f.Count())));
-            
+
             // 6. Output number of books per authors and films per director
             //Console.WriteLine(string.Join(Environment.NewLine,
             //    data.OfType<ArtObject>().GroupBy(f => f.Author).Select(f => f.Key + ": " + f.Count())));
 
+            // 7. Output how many different letters used in all actors names
+            //Console.WriteLine(string.Join(Environment.NewLine,
+            //    data.OfType<Film>().SelectMany(f => f.Actors).Select(u => u.Name).Distinct().Select(f => f.Distinct().Count())));
+
+            // 8. Output names of all books ordered by names of their authors and number of pages
+            //Console.WriteLine(string.Join(Environment.NewLine,
+            //    data.OfType<Book>().OrderBy(f => f.Author).OrderBy(f => f.Pages).Select(f => f.Name)));
+
+            // 9. Output actor name and all films with this actor
+            //foreach (var item in
+            //        data.OfType<Film>().SelectMany(f => f.Actors).GroupBy(f => f.Name,
+            //            f => data.OfType<Film>().Where(i => i.Actors.Contains(f)).Select(i => i.Name)))
+            //{
+            //    Console.WriteLine(item.Key);
+            //    foreach (var inner in item.SelectMany(f => f))
+            //    {
+            //        Console.WriteLine("\t" + inner);
+            //    }
+            //}
+
+            // 10. Output sum of total number of pages in all books and all int values inside all sequences in data
+            //Console.WriteLine(data.OfType<Book>().Select(f => f.Pages).Sum() + " " +
+            //    data.OfType<IEnumerable<int>>().SelectMany(f => f.OfType<int>()).Sum());
+
+            //11.Get the dictionary with the key - book author, value - list of author's books
+            //var dictionary = data.OfType<Book>().GroupBy(f => f.Author,
+            //            f => data.OfType<Book>().Where(i => i.Author.Equals(f.Author)).ToList());
+            //foreach (var item in dictionary)
+            //{
+            //    Console.WriteLine(item.Key);
+            //    foreach (var inner in item.SelectMany(f => f).Distinct())
+            //    {
+            //        Console.WriteLine("\t" + inner.Name);
+            //    }
+            //}            
+
+            // 12. Output all films of "Matt Damon" excluding films with actors whose name are presented in data as strings
+            //Console.WriteLine(string.Join(Environment.NewLine,
+            //    data.OfType<Film>().SelectMany(f => f.Actors).Select(f => f.Name).Except(data.OfType<string>())));
             
         }
     }
