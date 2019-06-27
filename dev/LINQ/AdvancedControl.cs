c8407398fa00654bd73ae0a38b36aa190751cdae
Kate <32255660+YanchukK@users.noreply.github.com>
2019-06-27 20:50:54 +0300

    Update AdvancedControl.cs

--- dev/LINQ/AdvancedControl.cs
+++ dev/LINQ/AdvancedControl.cs
@@ -30,11 +30,12 @@ namespace AdvancedControl
 
         static void Main(string[] args)
         {
-            var books = new List<Book>() {
-                        new Book() { Name = "Guards! Guards!", Year = 1810 },
-                        new Book() { Name="Finders Keepers", Year = 2002 },
-                        new Book() { Name="Finders LINQ Keepers", Year = 2016 }
-                    };
+            var books = new List<Book>()
+            {
+                new Book() { Name = "Guards! Guards!", Year = 1810 },
+                new Book() { Name="Finders Keepers", Year = 2002 },
+                new Book() { Name="Finders LINQ Keepers", Year = 2016 }
+            };
 
             // Выберите все книги, в названии которых есть слово LINQ, а год издания высокосный
             var resultBooks = books.Where(f => f.Name.Contains("LINQ") && IsLeapYear(f.Year));
@@ -42,26 +43,25 @@ namespace AdvancedControl
             // Дана последовательность русских слов.
             // Сколько букв алфавита понадобилось для написания этих слов
             Console.OutputEncoding = Encoding.UTF8; // Поддержка кириллицы
-            string[] words = { "Ниссан", "Альфа Ромео", "БМВ"};
-            var countOfLetters = words.SelectMany(f => f.Trim().ToArray()).ToArray().Distinct().Count();
-
+            string[] words = { "Ниссан", "АльСсСсфа Ромео", "БМВ"};
+            var countOfLetters = words.SelectMany(f => f.Trim(' ').ToUpper()).Distinct().Count();
 
             // Дан массив целых двузначных чисел. Упорядочить их по возрастанию
             // старшего разряда, а затем по убыванию младшего, например { 14, 12, 23, 20, 33, 32 }
-
             int[] array = { 32, 12, 14, 33, 23, 20 };
 
-            var newarray = array.OrderBy(f => f).GroupBy(f => f%10);
-
+            var newarray = array.OrderByDescending(f => f%10).OrderBy(f => f - (f%10));
 
             // Дан массив книг books. Сколько книг написал каждый автор (два столбца: Автор, Количество)
-            var newBooks = new List<BookWithAuthor>() {
-                        new BookWithAuthor() { Author = "Terry Pratchett", Name = "Guards! Guards!" },
-                        new BookWithAuthor() { Author = "Stephen King", Name="Finders Keepers" },
-                        new BookWithAuthor() { Author = "Stephen King", Name="Finders Keepers" }
-                    };
+            var newBooks = new List<BookWithAuthor>()
+            {
+                new BookWithAuthor() { Author = "Terry Pratchett", Name = "Guards! Guards!" },
+                new BookWithAuthor() { Author = "Stephen King", Name="Finders Keepers" },
+                new BookWithAuthor() { Author = "Stephen King", Name="Finders Keepers" }
+            };
 
-            var authorAndNumberOfBooks = newBooks.GroupBy(f => f.Author).Select(f => f.Key + ", " + f.Count());
+            Console.WriteLine(string.Join(Environment.NewLine,
+                newBooks.GroupBy(f => f.Author).Select(f => f.Key + ", " + f.Count())));
         }
     }
 }
